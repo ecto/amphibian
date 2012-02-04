1f4a23153ddca85ea71a9ac6cfacb3f77b181c6c
Cam Pedersen <diffference@gmail.com>
2012-02-04 01:34:39 -0500

    [minor] enforce cli usage

--- amphibian.js
+++ amphibian.js
@@ -5,12 +5,17 @@ var director = require('director');
 var broomstick = require('broomstick');
 var broom = new broomstick();
 var router = new director.http.Router();
-var argv = require('optimist')
-  .usage('Usage: $0 -p [num]')
+var optimist = require('optimist');
+var argv = optimist.usage('Pipe an SSH session to your browser\nUsage: $0 hostname options')
   .alias('p', 'port')
   .describe('p', 'Specify a port to run the webserver on')
   .default('p', 8080)
   .argv;
+
+if (!argv._[0]) {
+  optimist.showHelp();
+  process.exit();
+}
 
 router.get('/', broom);
 router.get('*', broom);
@@ -19,7 +24,7 @@ var server = http.createServer(function (req, res) {
   router.dispatch(req, res);
 });
 
-server.listen(argv.p || 8080);
+server.listen(argv.p);
 
 var io = require('socket.io').listen(server);
 
